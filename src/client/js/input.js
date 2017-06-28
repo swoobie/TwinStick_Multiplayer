806d89517ae416e5acce8da118e2dbfd94116a9c
swoobie <jdross91@gmail.com>
2017-06-27 23:03:54 -0600

    Fix untracked changed

--- src/client/js/input.js
+++ src/client/js/input.js
@@ -4,26 +4,26 @@ var Input = {
     if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
       Client.socket.emit('playerMove', {id: Player.id, direction: 'left'});
-      Player.move(Player.x - 1, Player.y);
-      console.log('Local Move: ' + Player.id + ' ' + Player.x + ' ' + Player.y  + ' left');
+      Player.rotate.left();
+      //console.log('Local Move: ' + Player.id + ' ' + Player.x + ' ' + Player.y  + ' left');
     }
     if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
       Client.socket.emit('playerMove', {id: Player.id, direction: 'right'});
-      Player.move(Player.x + 1, Player.y);
-      console.log('Local Move: ' + Player.id + ' ' + Player.x + ' ' + Player.y  + ' right');
+      Player.rotate.right();
+      //console.log('Local Move: ' + Player.id + ' ' + Player.x + ' ' + Player.y  + ' right');
     }
 	if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
     {
       Client.socket.emit('playerMove', {id: Player.id, direction: 'up'});
-      Player.move(Player.x, Player.y - 1);
-      console.log('Local Move: ' + Player.id + ' ' + Player.x + ' ' + Player.y + ' up');
+      Player.move.forward();
+      //console.log('Local Move: ' + Player.id + ' ' + Player.x + ' ' + Player.y + ' up');
     }
 	if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
     {
       Client.socket.emit('playerMove', {id: Player.id, direction: 'down'});
-      Player.move(Player.x, Player.y + 1);
-      console.log('Local Move: ' + Player.id + ' ' + Player.x + ' ' + Player.y + ' down');
+      Player.move.backward();
+      //console.log('Local Move: ' + Player.id + ' ' + Player.x + ' ' + Player.y + ' down');
     }
   }
 }
